ffdf732a86cd07abbd25639ed1cea2ace323186c
Dan6erbond <moravrav@gmail.com>
2020-07-14 13:45:31 +0200

    Add on_raw_reaction_remove handler

--- cogs/reaction_role_cog.py
+++ cogs/reaction_role_cog.py
@@ -15,8 +15,6 @@ class ReactionRoleCog(commands.Cog):
         if payload.user_id == bot.user.id:
             return
 
-        channel = self.bot.get_channel(payload.channel_id)
-        message = await channel.fetch_message(payload.message_id)
         emoji = payload.emoji.name if not payload.emoji.is_custom_emoji(
         ) else f"<:{payload.emoji.name}:{payload.emoji.id}>"
 
@@ -31,6 +29,28 @@ class ReactionRoleCog(commands.Cog):
             role = guild.get_role(react_role.role)
             try:
                 await member.add_roles(role)
+            except Exception as e:
+                self.bot.send_error(e)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
+        if payload.user_id == bot.user.id:
+            return
+
+        emoji = payload.emoji.name if not payload.emoji.is_custom_emoji(
+        ) else f"<:{payload.emoji.name}:{payload.emoji.id}>"
+
+        react_role = session.query(ReactionRole).filter(
+            and_(
+                ReactionRole.message_id == payload.message_id,
+                ReactionRole.emoji == emoji)).first()
+
+        if react_role:
+            guild = bot.get_guild(payload.guild_id)
+            member = guild.get_member(payload.user_id)
+            role = guild.get_role(react_role.role)
+            try:
+                await member.remove_roles(role)
             except Exception as e:
                 self.bot.send_error(e)
 
