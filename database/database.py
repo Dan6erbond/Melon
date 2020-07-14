85370db070f268fae89dcf619c6fc8d20e23bda2
Dan6erbond <moravrav@gmail.com>
2020-07-14 12:45:08 +0200

    Fix relationships
    Create tables in right order.

--- database/database.py
+++ database/database.py
@@ -9,7 +9,26 @@ from .models import *
 path = pathlib.Path(__file__).parent.absolute()
 engine = create_engine(f'sqlite:///{path}/database.db')
 
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+if __name__ == "__main__":
+    # order is important
+
+    Category.__table__.create(bind=engine)
+    Guild.__table__.create(bind=engine)
+    GuildCategory.__table__.create(bind=engine)
+
+    Channel.__table__.create(bind=engine)
+    ChannelEmoji.__table__.create(bind=engine)
+
+    Melon.__table__.create(bind=engine)
+    GuildMelon.__table__.create(bind=engine)
+    MelonCategory.__table__.create(bind=engine)
+    MelonTag.__table__.create(bind=engine)
+    Tag.__table__.create(bind=engine)
+
+    Message.__table__.create(bind=engine)
+    ReactionRole.__table__.create(bind=engine)
