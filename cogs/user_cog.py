7382cfba3d8f6d704e9b0461e46b5191c91931ec
Dan6erbond <moravrav@gmail.com>
2020-07-14 13:28:03 +0200

    Add option for extended info

--- cogs/user_cog.py
+++ cogs/user_cog.py
@@ -6,6 +6,8 @@ from typing import TYPE_CHECKING
 import discord
 from discord.ext import commands
 
+from const import EMOJIS
+
 if TYPE_CHECKING:
     from melon import Melon
 
@@ -37,10 +39,6 @@ class UserCog(commands.Cog):
     async def info(self, ctx: commands.Context, user: discord.Member = None):
         user = ctx.guild.get_member(ctx.author.id) if not user else user
 
-        msg = await ctx.send("Please wait while we find the data...")
-
-        time_started = datetime.now()
-
         embed = self.bot.embed
         embed.colour = user.colour
         embed.set_author(name="User information for {0.nick}".format(user))
@@ -54,7 +52,50 @@ class UserCog(commands.Cog):
         roles = [" - {}".format(r.name) for r in user.roles]
         embed.add_field(name="Roles in the guild", value="\n".join(roles), inline="False")
 
-        msg.edit(content="", embed=embed)
+        msg = await ctx.send(content=f"Here's the information for {user.mention}:", embed=embed)
+
+        msg1 = await ctx.send("Basic information found, fetch more?")
+        await msg1.add_reaction(EMOJIS["CHECK"])
+
+        def check(r, u):
+            return u.id == ctx.author.id
+
+        try:
+            reaction = await self.bot.wait_for("reaction_add", check=check, timeout=2 * 60)
+            if reaction[0].custom_emoji and reaction[0].emoji.id == int(EMOJIS["CHECK"].split(":")[2]):
+                await msg1.edit(content="Please wait while I fetch the data...")
+                time_started = datetime.now()
+
+                count = 0
+                long_count = 0
+                char_count = 0
+                word_count = 0
+                for channel in user.guild.channels:
+                    channel_count = 0
+                    permissions = channel.permissions_for(user)
+                    if not permissions.read_messages:
+                        continue
+                    if not isinstance(channel, discord.TextChannel):
+                        continue
+                    async for msg in channel.history(limit=None):
+                        if msg.author.id == user.id:
+                            count += 1
+                            channel_count += 1
+                            chars = len(msg.content)
+                            char_count += chars
+                            word_count += len(msg.content.split(" "))
+                            if chars > 5:
+                                long_count += 1
+
+                embed.add_field(name="Messages written to the guild", value=str(count), inline=False)
+                embed.add_field(name="Messages longer than 5 characters", value=str(long_count), inline=False)
+                embed.add_field(name="Characters written", value=str(char_count), inline=False)
+                embed.add_field(name="Words written", value=str(word_count), inline=False)
+
+                await msg.edit(content="", embed=embed)
+                await msg1.edit(content=f"Data found! Took me {datetime.now() - time_started}.")
+        except Exception as e:
+            await self.bot.send_error(e)
 
 
 def setup(bot):
