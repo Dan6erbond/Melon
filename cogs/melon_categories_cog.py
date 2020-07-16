fb7771142139b63d90a8078017d9552e071c2989
Dan6erbond <moravrav@gmail.com>
2020-07-16 08:58:51 +0200

    Squashed commit of the following:
    commit 977d36d373bf42a7f660e3fe10bdddd058b2b795
    Author: Dan6erbond <moravrav@gmail.com>
    Date:   Thu Jul 16 08:58:30 2020 +0200
    
        Fix usage of f-strings and `get_guild`
    
    commit 7c059ba15949163b2bfefe4dc2969cfaffb26629
    Author: Dan6erbond <moravrav@gmail.com>
    Date:   Thu Jul 16 08:57:11 2020 +0200
    
        Implement !enablecat and !disablecat
    
         - Update typing in `!delcat`.
         - Move `get_guild()` to `class Melon`.
    
    commit 5b864ed3f7773978ed8acb3e9d6f65206d836a5f
    Author: Dan6erbond <moravrav@gmail.com>
    Date:   Thu Jul 16 08:47:36 2020 +0200
    
        Fix record delete
    
    commit bd47dc61cc17313893ae36712c3a358f98684019
    Author: Dan6erbond <moravrav@gmail.com>
    Date:   Thu Jul 16 08:46:17 2020 +0200
    
        Implement !delcat
    
    commit aacfbe3c34016e109f3c2b1ab61105e87dd53153
    Author: Dan6erbond <moravrav@gmail.com>
    Date:   Thu Jul 16 08:41:46 2020 +0200
    
        Move !addcat to MelonCategoriesCog and setup

---
+++ cogs/melon_categories_cog.py
@@ -0,0 +1,97 @@
+import asyncio
+
+import discord
+from discord.ext import commands
+from sqlalchemy import func
+
+from const import EMOJIS
+from database.database import session
+from database.models import Category
+from helpers import is_authorized
+
+
+class MelonCategoriesCog(commands.Cog):
+    def __init__(self, bot: 'Melon'):
+        self.bot = bot
+
+    @commands.command(help="Add a Melon category.")
+    @commands.check(is_authorized)
+    async def addcat(self, ctx: commands.Context, *, cat: str):
+        category = session.query(Category).filter(func.lower(Category.name) == cat.lower()).first()
+
+        if category:
+            await ctx.send(f"<{EMOJIS['XMARK']}> '{cat}' is already a category in the Melons!")
+        else:
+            category = Category(name=cat)
+            session.add(category)
+            session.commit()
+            await ctx.send(f"<{EMOJIS['CHECK']}> Successfully added '{cat}' as a category in the Melons!")
+
+    @commands.command(help="Delete a Melon category.")
+    @commands.check(is_authorized)
+    async def delcat(self, ctx: commands.Context, *, cat: str):
+        category = session.query(Category).filter(func.lower(Category.name) == cat.lower()).first()
+
+        if not category:
+            await ctx.send(f"<{EMOJIS['XMARK']}> '{cat}' isn't a category in the Melons!")
+            return
+
+        message = await ctx.send(f"❓ Are you sure you want to remove {cat} from the Melons?")
+        await message.add_reaction(EMOJIS['CHECK'])
+        await message.add_reaction(EMOJIS['XMARK'])
+
+        try:
+            reaction = await self.bot.wait_for("reaction_add",
+                                               check=lambda r, u: u.id == ctx.author.id and r.message.id == message.id,
+                                               timeout=2 * 60)
+        except asyncio.exceptions.TimeoutError:
+            await ctx.send(f"<{EMOJIS['XMARK']}> That took too long! " +
+                           "You can restart the process by calling this command again.")
+            return
+        finally:
+            if reaction[0].custom_emoji and reaction[0].emoji.id == int(EMOJIS['CHECK'].split(":")[2]):
+                session.delete(category)
+                session.commit()
+                await ctx.send(f"<{EMOJIS['CHECK']}> Successfully removed '{cat}' as a category in the Melons!")
+
+    @commands.command(help="Enable a Melon category in this guild.")
+    @commands.has_permissions(administrator=True)
+    async def enablecat(self, ctx: commands.Context, *, cat: str):
+        category = session.query(Category).filter(func.lower(Category.name) == cat.lower()).first()
+
+        if not category:
+            await ctx.send(f"<{EMOJIS['XMARK']}> Melon category '{cat}' doesn't exist!")
+            return
+
+        guild = self.bot.get_guild(ctx.guild.id)
+
+        category_ids = [cat.category_id for cat in guild.categories]
+        if category.category_id in category_ids:
+            await ctx.send(f"❗ Melon category '{cat}' was already enabled!")
+        else:
+            guild.categories.append(category)
+            session.commit()
+            await ctx.send(f"<{EMOJIS['CHECK']}> Melon category '{cat}' successfully enabled!")
+
+    @commands.command(help="Disable a Melon category in this guild.")
+    @commands.has_permissions(administrator=True)
+    async def disablecat(self, ctx, cat):
+        category = session.query(Category).filter(func.lower(Category.name) == cat.lower()).first()
+
+        if not category:
+            await ctx.send(f"<{EMOJIS['XMARK']}> Melon category '{cat}' doesn't exist!")
+            return
+
+        guild = self.bot.get_guild(ctx.guild.id)
+
+        category_ids = [cat.category_id for cat in guild.categories]
+        if category.category_id in category_ids:
+            guild.categories.remove(category)
+            session.commit()
+            await ctx.send(f"<{EMOJIS['CHECK']}> Melon category '{cat}' successfully disabled!")
+        else:
+            await ctx.send(f"<{EMOJIS['XMARK']}> Melon category '{cat}' was never enabled in this guild!")
+
+
+def setup(bot: 'Melon'):
+    bot.add_cog(MelonCategoriesCog(bot))
--- melon.py
+++ melon.py
@@ -8,6 +8,9 @@ import discord
 from discord.ext import commands
 
 from const import MAINTAINER, VERSION
+from database.database import session
+from database.models import Category, Guild
+from sqlalchemy.sql.expression import true
 
 
 class Melon(commands.Bot):
@@ -40,6 +43,17 @@ class Melon(commands.Bot):
 
         return embed
 
+    def get_guild(self, guild_id: int) -> Guild:
+        guild = session.query(Guild).filter(Guild.guild_id == guild_id).first()
+
+        if not guild:
+            categories = session.query(Category).filter(Category.default == true()).all()
+            guild = Guild(guild_id=guild_id, categories=categories)
+            session.add(guild)
+            session.commit()
+
+        return guild
+
     async def get_message(self, id: int, channel: discord.ChannelType = None):
         try:
             message = await channel.fetch_message(id)
@@ -62,7 +76,7 @@ class Melon(commands.Bot):
 
 
 extensions = ["cogs.util_cog", "cogs.poll_cog", "cogs.user_cog",
-              "cogs.reaction_role_cog", "cogs.melon_cog"]
+              "cogs.reaction_role_cog", "cogs.melon_cog", "cogs.melon_categories_cog"]
 
 if __name__ == "__main__":
     bot = Melon()
